a86f8a91c28d7fe383e8f0d0545c80135f6b486a
Duong Quoc Viet <ntysux@gmail.com>
2023-07-01 05:37:51 +0700

    create: dashboard ui

---
+++ components/dashboard.tsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+  return(
+    <>
+      <div className="flex gap-x-3 xl:max-w-5xl mx-3 sm:mx-auto">
+        <button className="p-3 rounded-lg shadow-md shadow-neutral-200/75 text-sm font-normal hover:shadow-lg">
+          Món mới
+        </button>
+        <button className="p-3 rounded-lg shadow-md shadow-neutral-200/75 text-sm font-normal hover:shadow-lg">
+          +0 được chọn
+        </button>
+      </div>
+    </>
+  )
+}
