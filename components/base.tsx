2654b94c915625501a6a5e09b156d23aa94aae38
Duong Quoc Viet <ntysux@gmail.com>
2023-07-01 09:57:56 +0700

    chore: ui update

--- components/base.tsx
+++ components/base.tsx
@@ -5,7 +5,7 @@ export default function Base({
 }) {
   return (
     <>
-      <div className="max-w-5xl my-3 mx-3 grid grid-cols-1 gap-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:mx-auto">
+      <div className="max-w-5xl my-3 mx-3 grid grid-cols-1 gap-3 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:mx-auto">
         {children}
       </div>
     </>
--- components/card.tsx
+++ components/card.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import { Data } from "@/app/menu/page"
+import { Checkbox } from "@chakra-ui/react"
 
 export default function Card({food}: {food: Data}) {
   return (
     <>
-      <div className="space-y-3 p-3 rounded-lg hover:ring-1 hover:ring-neutral-200 hover:ring-inset">
-        <div className="py-9 bg-neutral-200/50 rounded-lg" />
+      <div className="flex justify-between items-center p-3 rounded-lg hover:ring-1 hover:ring-neutral-200 hover:ring-inset">
         <div>
           <p className="text-sm font-bold text-neutral-700">
             {food.name}
@@ -12,6 +14,10 @@ export default function Card({food}: {food: Data}) {
           <i className="text-xs text-neutral-600">bởi <span className="font-medium">
             {food.author}
           </span></i>
+        </div>
+
+        <div>
+          <Checkbox variant='filled' size='lg' />
         </div>
       </div>
     </>
