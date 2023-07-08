b8d08115ec4b6b4f1e4cb34b6ee884a8a2be2f62
Duong Quoc Viet <ntysux@gmail.com>
2023-07-08 20:52:20 +0700

    chore: ui update

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,15 +1,15 @@
 import { usePathname } from "next/navigation"
 import Banner from "./banner"
 import Dashboard from "./dashboard"
-import NavRoutesMobile from "./nav.routes.mobile"
 import NavRoutes from "./nav.routes"
+import NavRoutesMobile from "./nav.routes.mobile"
 
 export default function Nav() {
   const pathname = usePathname()
 
   return (
     <header className="bg-white sticky top-0 z-10">
-      <nav className="flex justify-between items-center mx-auto py-5 px-3 xl:px-0 w-screen max-w-5xl">
+      <nav className="flex justify-between items-center py-5">
         <h1 className="text-xl text-neutral-800 tracking-widest">
           Menoú
         </h1>
