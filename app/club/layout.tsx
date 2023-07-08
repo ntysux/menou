fdf8ea6c08bf487db5805249d3501508e7b4efed
Duong Quoc Viet <ntysux@gmail.com>
2023-07-08 21:55:45 +0700

    add a layout using base container

---
+++ app/club/layout.tsx
@@ -0,0 +1,13 @@
+export default function ClubLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <div className="mx-3 sm:w-screen sm:max-w-5xl sm:mx-auto">
+        {children}
+      </div>
+    </>
+  )
+}
