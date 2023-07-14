8f857e1c95815e82fe711ea31fe3382e2490ab76
Duong Quoc Viet <ntysux@gmail.com>
2023-07-14 14:29:10 +0700

    unify: colors & actions

--- components/card.actions.tsx
+++ components/card.actions.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { Dialog, Transition, Popover } from '@headlessui/react'
-import { IconColorSwatch, IconPencil, IconTrash } from '@tabler/icons-react'
+import { IconColorSwatch, IconPencil, IconTrash, IconX } from '@tabler/icons-react'
 import { Fragment, useState } from 'react'
 import { motion } from "framer-motion"
+import { Divider } from '@chakra-ui/react'
 
 const container = {
   visible: {
@@ -27,7 +28,8 @@ export default function CardActions({
 }: {
   children: (setState: React.Dispatch<React.SetStateAction<boolean>>) => React.ReactNode
 }) {
-  const [isActionsOpen, setActionsOpen] = useState(false)
+  const [isActionsOpen, setActionsOpen] = useState<boolean>(false)
+  const [isColorShow, setColorShow] = useState<boolean>(false)
 
   return (
     <>
@@ -58,50 +60,42 @@ export default function CardActions({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="flex items-center space-x-1 p-1 px-5 rounded-xl bg-neutral-800/75 backdrop-blur-[1px] shadow-xl transition-all">
-                  
-
-
-
-                  <Popover className="relative">
-                    <Popover.Button as='button' className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
-                      <IconColorSwatch size='18px' strokeWidth='2.5' />
-                    </Popover.Button>
-                    <Popover.Overlay className="fixed inset-0" />
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="opacity-0 translate-y-2"
-                      enterTo="opacity-100 translate-y-0"
-                      leave="transition ease-in duration-150"
-                      leaveFrom="opacity-100 translate-y-0"
-                      leaveTo="opacity-0 translate-y-2"
-                    >
-                      <Popover.Panel className="absolute left-0 mt-5 z-10 p-2 rounded-full bg-neutral-800/75 backdrop-blur-[1px]">
-                        <motion.ul
-                          className='list-none flex space-x-3'
-                          variants={container}
-                          initial="hidden"
-                          animate="visible"
+                <Dialog.Panel className={`flex items-center space-x-1 p-1 rounded-full bg-neutral-800/75 backdrop-blur-[1px] shadow-xl transition-all`}>
+                  {isColorShow 
+                    ?
+                      <motion.ul
+                        className='list-none flex items-center space-x-1'
+                        variants={container}
+                        initial="hidden"
+                        animate="visible"
+                      >
+                        {['bg-[#ec4899]', 'bg-[#8b5cf6]', 'bg-[#2dd4bf]', 'bg-white'].map((color, key) => 
+                          <motion.li key={key} variants={item}>
+                            <div className={`p-2 m-1 ${color} rounded-full`} />
+                          </motion.li>
+                        )}
+                        <motion.li 
+                          onClick={() => setColorShow(false)}
+                          className='text-white p-1 rounded-full bg-neutral-700'
                         >
-                          {['bg-[#ec4899]', 'bg-[#8b5cf6]', 'bg-[#2dd4bf]', 'bg-white'].map((color, key) => 
-                            <motion.li key={key} variants={item}>
-                              <div className={`p-2 ${color} rounded-full`} />
-                            </motion.li>
-                          )}
-                        </motion.ul>
-                      </Popover.Panel>
-                    </Transition>
-                  </Popover>
-
-
-                  
-                  <button className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
-                    <IconPencil size='18px' strokeWidth='2.5' />
-                  </button>
-                  <button className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
-                    <IconTrash size='18px' strokeWidth='2.5' />
-                  </button>
+                          <IconX size='16px' strokeWidth='2.7' />
+                        </motion.li>
+                      </motion.ul>
+                    :
+                      <>
+                        <button onClick={() => setColorShow(true)} className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
+                          <IconColorSwatch size='18px' strokeWidth='2.5' />
+                        </button>
+                        <Divider orientation='vertical' h='10px' rounded='full' borderColor='whiteAlpha.400' />
+                        <button className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
+                          <IconPencil size='18px' strokeWidth='2.5' />
+                        </button>
+                        <Divider orientation='vertical' h='10px' rounded='full' borderColor='whiteAlpha.400' />
+                        <button className='p-2 text-white rounded-full hover:bg-neutral-500/40'>
+                          <IconTrash size='18px' strokeWidth='2.5' />
+                        </button>
+                      </>
+                  }
                 </Dialog.Panel>
               </Transition.Child>
             </div>
