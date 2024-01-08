ce65b0e5833c6b978ade4fba6cf125939bc2e5bd
Duong Quoc Viet <ntysux@gmail.com>
2024-01-09 01:50:37 +0700

    use quicksand font

---
+++ app/_ui/font.ts
@@ -0,0 +1,5 @@
+import { Quicksand } from 'next/font/google'
+
+const quicksand = Quicksand({subsets: ['latin', 'vietnamese']})
+
+export default quicksand
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import quicksand from './_ui/font'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -10,13 +8,15 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={quicksand.className}>
+        {children}
+      </body>
     </html>
   )
 }
