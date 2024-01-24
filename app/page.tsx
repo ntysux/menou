0bd487f1b81b69c97deb1fc20da16139cdd723fc
Duong Quoc Viet <ntysux@gmail.com>
2024-01-24 22:09:48 +0700

    style update & add theme control

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,128 @@
-import { IconArrowNarrowRight, IconBackground, IconVersions } from "@tabler/icons-react"
+import { IconArrowNarrowRight, IconBackground, IconTags } from "@tabler/icons-react"
 import Card from "./_ui/card"
+import Link from "next/link"
+import Theme from "./_ui/theme.control"
+import { cookies } from "next/headers"
+
+const routes: string[] = [
+  'Trang chủ',
+  'Hỗ trợ',
+  'Đánh giá'
+]
+
+const category: string[] = [
+  'Ẩm thực đường phố',
+  'Đặc sản địa phương',
+  'Món ăn gia đình',
+  'Kĩ năng nấu nướng',
+  'Bí quyết / mẹo'
+]
 
 export default function Page() {
+  const cookieThemeValue = cookies().get('theme')?.value
+  const darkMode = typeof cookieThemeValue === 'string' && cookieThemeValue === 'dark'
+
   return (
-    <>
-      <div className="min-h-screen grid grid-cols-4 gap-9">
-        <div className="border-r border-r-neutral-400 border-dashed p-9 flex flex-col gap-11 divide-y divide-neutral-400 divide-dashed">
-          <div className="flex-1 flex flex-col justify-between">
-            <h1 className="text-neutral-800 text-xs font-bold tracking-widest">
-              MENOÚ
-            </h1>
-            <div className="flex flex-col items-start gap-3">
-              <button className="text-xl text-neutral-800 font-extrabold">
-                Trang chủ
+    <div className="min-h-screen grid grid-cols-4 gap-9">
+      {/* col 1 */}
+      <article className="p-9 flex flex-col gap-11 border-r border-dashed border-r-neutral-400 divide-y divide-dashed divide-neutral-400 dark:border-neutral-500 dark:divide-neutral-500">
+        <div className="flex-1 flex flex-col justify-between">
+          <h1 className="text-xs text-neutral-800 font-bold tracking-widest dark:text-white">
+            MENOÚ
+          </h1>
+          <div className="flex flex-col items-start gap-3">
+            {routes.map((route, index) =>
+              <button 
+                key={index} 
+                className="outline-none text-xl text-neutral-800 font-extrabold dark:text-white"
+              >
+                {route}
               </button>
-              <button className="text-xl text-neutral-800 font-extrabold">
-                Hỗ trợ
-              </button>
-              <button className="text-xl text-neutral-800 font-extrabold">
-                Đánh giá
-              </button>
-            </div>
+            )}
           </div>
-          <div className="py-5">
-            <p className="text-xs text-neutral-500 font-medium">
-              Bản quyền © thuộc về Menoú 
+        </div>
+        <div className="py-5">
+          <p className="text-xs text-neutral-500 font-medium">
+            Bản quyền © thuộc về Menoú 
+          </p>
+        </div>
+      </article>
+      <div className="col-span-3 grid grid-cols-3 divide-x divide-dashed divide-neutral-400 transition-all duration-200 dark:divide-neutral-600 dark:bg-neutral-800 dark:m-3 dark:rounded-xl">
+        {/* col 2 */}
+        <article className="py-9 px-3 flex flex-col justify-between">
+          <div className="flex items-center gap-3">
+            <IconTags className="text-neutral-800 dark:text-neutral-500" />
+            <h2 className="text-sm text-neutral-400 font-bold dark:text-neutral-300">
+              Danh mục
+            </h2>
+          </div>
+          <div className="flex flex-col items-start divide-y divide-dashed divide-neutral-400 dark:divide-neutral-600">
+            {category.map((item, index) =>
+              <div key={index} className="py-7 w-full">
+                <button className="block outline-none text-neutral-800 font-bold dark:text-white">
+                  {item}
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="text-center">
+            <button className="outline-none">
+              <IconArrowNarrowRight 
+                size='20px'
+                className="text-neutral-800 dark:text-neutral-300"
+              />
+            </button>
+          </div>
+        </article>
+        {/* col 3 */}
+        <article className="py-9 px-3 flex flex-col justify-between">
+          <div className="flex items-center gap-3">
+            <IconBackground className="text-neutral-800 dark:text-neutral-500" />
+            <h2 className="text-sm text-neutral-400 font-bold dark:text-neutral-300">
+              Được tạo gần đây
+            </h2>
+          </div>
+          <div className="space-y-5">
+            <Card saveBtn />
+            <Card goBtn />
+            <Card goBtn />
+          </div>
+          <div className="text-center">
+            <button className="outline-none">
+              <IconArrowNarrowRight 
+                size='20px'
+                className="text-neutral-800 dark:text-neutral-300"
+              />
+            </button>
+          </div>
+        </article>
+        {/* col 4 */}
+        <article className="p-9 flex flex-col items-start justify-between">
+          <div className="w-full text-right">
+            <Link 
+              href='/auth' 
+              className="outline-none text-sm text-neutral-800 font-medium hover:underline dark:text-white"
+            >
+              Đăng nhập
+            </Link>
+          </div>
+          <div className="space-y-3">
+            <p className="text-sm text-neutral-500 font-medium">
+              Bạn là người mới ?
+              Khám phá ứng dụng và các tính năng với hướng dẫn của chúng tôi.
             </p>
+            <button className="outline-none flex items-center group">
+              <span className="text-xl text-neutral-800 font-bold dark:text-white">
+                Bắt đầu
+              </span>
+              <IconArrowNarrowRight className="text-neutral-800 transition-transform duration-300 group-hover:translate-x-3 dark:text-white" />
+            </button>
           </div>
-        </div>
-        <div className="col-span-3 grid grid-cols-3 divide-x divide-neutral-400 divide-dashed">
-          <div className="py-9 px-3 flex flex-col justify-between">
-            <div className="flex items-center gap-3">
-              <IconVersions className="text-neutral-800" />
-              <h2 className="text-sm text-neutral-400 font-medium">
-                Danh mục
-              </h2>
-            </div>
-            <div className="flex flex-col items-start divide-y divide-neutral-400 divide-dashed">
-              <div className="py-7 w-full">
-                <button className="block text-neutral-800 font-extrabold">
-                  Ẩm thực đường phố
-                </button>
-              </div>
-              <div className="py-7 w-full">
-                <button className="block text-neutral-800 font-extrabold">
-                  Đặc sản địa phương
-                </button>
-              </div>
-              <div className="py-7 w-full">
-                <button className="block text-neutral-800 font-extrabold">
-                  Món ăn gia đình
-                </button>
-              </div>
-              <div className="py-7 w-full">
-                <button className="block text-neutral-800 font-extrabold">
-                  Kĩ năng nấu nướng
-                </button>
-              </div>
-              <div className="py-7 w-full">
-                <button className="block text-neutral-800 font-extrabold">
-                  Bí quyết / mẹo
-                </button>
-              </div>
-            </div>
-            <div className="text-center">
-              <button className="outline-none">
-                <IconArrowNarrowRight 
-                  size='20px'
-                  className="text-neutral-800"
-                />
-              </button>
-            </div>
+          <div className="flex justify-end w-full">
+            <Theme mode={Boolean(darkMode)} />
           </div>
-
-          <div className="py-9 px-3 flex flex-col justify-between">
-            <div className="flex items-center gap-3">
-              <IconBackground className="text-neutral-800" />
-              <h2 className="text-sm text-neutral-400 font-medium">
-                Được tạo gần đây
-              </h2>
-            </div>
-            <div className="space-y-5">
-              <Card goBtn />
-              <Card goBtn />
-              <Card goBtn />
-            </div>
-            <div className="text-center">
-              <button className="outline-none">
-                <IconArrowNarrowRight 
-                  size='20px'
-                  className="text-neutral-800"
-                />
-              </button>
-            </div>
-          </div>
-
-          <div className="p-9 flex flex-col items-start justify-between">
-            <div className="text-right">
-              <button className="outline-none text-sm text-neutral-800 font-medium hover:underline">
-                Đăng nhập
-              </button>
-            </div>
-            <div className="space-y-3">
-              <p className="text-sm text-neutral-500 font-medium">
-                Bạn là người mới ?
-                Tạo công thức nấu ăn đầu tiên với hướng dẫn của chúng tôi.
-              </p>
-              <button className="outline-none flex items-center group">
-                <span className="text-xl text-neutral-800 font-bold">
-                  Bắt đầu
-                </span>
-                <IconArrowNarrowRight className="text-neutral-800 transition-transform duration-300 group-hover:translate-x-3" />
-              </button>
-            </div>
-            <div>
-
-            </div>
-          </div>
-        </div>
+        </article>
       </div>
-    </>
+    </div>
   )
 }
